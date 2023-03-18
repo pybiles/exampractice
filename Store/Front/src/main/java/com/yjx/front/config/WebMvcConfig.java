e0a3f91e8d1ce417147fb9808b18cc2cd79e8f3d
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-18 20:33:11 +0800

    前台代码 WebMvcConfig

---
+++ Store/Front/src/main/java/com/yjx/front/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.yjx.front.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    //直接注册一些ViewController
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //WebMvcConfigurer.super.addViewControllers(registry);
+
+        //配置  访问urlPath时对应的视图模板
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+
+    }
+}
