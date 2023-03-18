0457c971944974407041f13ddd79f7aa9d6531bf
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-18 10:59:12 +0800

    MybatisPlus结合注解annotation实现sql操作

--- Demos/MyBatisPlusDemo/src/main/java/com/yjx/mapper/UserMapper.java
+++ Demos/MyBatisPlusDemo/src/main/java/com/yjx/mapper/UserMapper.java
@@ -2,11 +2,22 @@ package com.yjx.mapper;
 
 import com.yjx.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper   //指定当前类是需要mybatis处理的mapper类
 public interface UserMapper {
 
-    public User getUserByName(String name);
+    User getUserByName(String name);
+
+    //    @Select()
+    //    @Delete()
+    //    @Update()
+    //    @Insert()
+    @Select("select * from user where email like '%${keyword}%'")
+    //根据关键字查询user信息
+    List<User> likeEmail(String keyword);
 
 
 }
--- Demos/MyBatisPlusDemo/src/test/java/com/yjx/shopping/MyBatisPlusDemoMainTest.java
+++ Demos/MyBatisPlusDemo/src/test/java/com/yjx/shopping/MyBatisPlusDemoMainTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class MyBatisPlusDemoMainTest {
 
@@ -13,12 +15,16 @@ public class MyBatisPlusDemoMainTest {
     UserMapper userMapper;
 
     @Test
-    void testMapper(){
+    void testMapper() {
 
         User tom = userMapper.getUserByName("Tom");
         System.out.println(tom);
-
     }
 
+    @Test
+    void testAnnotation() {
+        List<User> userList = userMapper.likeEmail("bao");
+        System.out.println(userList);
+    }
 
 }
