64d6011254cbfc7209fd5823b145af6af0ffc428
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-22 17:51:06 +0800

    后端页面 精简结构 引入登录页

--- Store/Behind/belind/src/router/index.js
+++ Store/Behind/belind/src/router/index.js
@@ -1,23 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+
+import Login from "@/shopping/Login.vue";
+
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  //我们在这里配置自己的路由规则
+  {path:"/",name:"Login",component:Login},
+
 ]
 
 const router = new VueRouter({
