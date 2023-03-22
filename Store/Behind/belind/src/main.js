7b75ba57b8b5856fc3ec8b549c8b73ec48f38575
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-22 17:58:29 +0800

    后台 前端 main.js中导入ElementUI

--- Store/Behind/belind/src/main.js
+++ Store/Behind/belind/src/main.js
@@ -4,6 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 
 Vue.config.productionTip = false
+//导入ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
