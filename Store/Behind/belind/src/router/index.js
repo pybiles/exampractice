e555bc43dae7328fa61e1bbb26b64fff3a8f8600
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-23 22:02:02 +0800

    后台 前端路由拦截

--- Store/Behind/belind/src/router/index.js
+++ Store/Behind/belind/src/router/index.js
@@ -10,20 +10,60 @@ import Home from "@/shopping/Home.vue";
 import Overview from "@/shopping/Overview.vue";
 
 const routes = [
-  //我们在这里配置自己的路由规则
-  {path:"/",name:"Login",component:Login},
-  {path: "/TestElementUI",name: "TestElementUI",component: TestElementUI},
-  {path:"/Home",name:"Home",component:Home,
-    children:[
-      {path:"/",name:"Overview",component:Overview},
-    ]
-  },
+    //我们在这里配置自己的路由规则
+    {path: "/", name: "Login", component: Login},
+    {path: "/TestElementUI", name: "TestElementUI", component: TestElementUI},
+    {path: "/Login", name: "Login", component: Login},
+    {
+        path: "/Home", name: "Home", component: Home,
+        children: [
+            {path: "/", name: "Overview", component: Overview},
+        ]
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+//路由拦截
+/*
+ to  目标路由对象
+ from  起始路由对象
+ next  执行视图切换的回调函数
+      如果不传参 执行from到to
+      如果传参,则到指定路由去
+ */
+router.beforeEach((to, from, next) => {
+
+    console.log(from)
+    console.log(to)
+    console.log(next)
+
+    //如果是到登录页,那就直接放行
+    if (to.path == "/" || to.path == "/Login") {
+        next();
+        return;
+    }
+
+    //到其它页面需要检查是否已经登录
+    let token = sessionStorage.getItem("token");
+    if (token) {
+        //有token,认为已经是登录状态,直接放行
+        next()
+    } else {
+        //没有token,到登录页去
+        next("/")
+        //location.href = "/"
+    }
+
+})
+
+router.afterEach((to, from) => {
+    console.log(from)
+    console.log(to)
+
 })
 
 export default router
