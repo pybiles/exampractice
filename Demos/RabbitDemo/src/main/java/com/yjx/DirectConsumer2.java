433b244381b96b5afa36b5a7d105e6a2f4005af9
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-23 16:16:29 +0800

    RabbitMQ  确保消息不丢失的措施 手动开启ack

--- Demos/RabbitDemo/src/main/java/com/yjx/DirectConsumer2.java
+++ Demos/RabbitDemo/src/main/java/com/yjx/DirectConsumer2.java
@@ -1,5 +1,6 @@
 package com.yjx;
 
+
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-public class DirectConsumer {
+public class DirectConsumer2 {
 
     @RabbitListener(queues = "directQueueDemo2")
     public void onMsg(String msg, Message message, Channel channel) throws IOException {
@@ -16,13 +17,14 @@ public class DirectConsumer {
         //默认是自动ack, 方法执行完成之后,自动向MQ队列发送一个ack确认,然后队列就认为消息被正常消费了,然后将消息从队列中删除.
 
         try {
-            System.out.println("DirectConsumer 接收到消息 " + msg);
+            System.out.println("DirectConsumer2 接收到消息 " + msg);
 
             int a = 3 / 0;
         }catch (Exception e){
-            //log ...
+            //log........
         }
 
+        //手动ack
         channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
 
     }
