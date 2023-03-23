9db8d44f5589856ca2ce1882f64cba6326e678bb
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-23 16:14:51 +0800

    RabbitMQ  确保消息不丢失的措施 Durable

--- Demos/RabbitDemo/src/main/java/com/yjx/ExchangeDirectConfig.java
+++ Demos/RabbitDemo/src/main/java/com/yjx/ExchangeDirectConfig.java
@@ -12,14 +12,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ExchangeDirectConfig {
 
+
+    @Bean
+    public Queue noDurableQueue(){
+        return new Queue("noDurableQueue",false);
+    }
+
     @Bean
     public DirectExchange directExchangeDemo2(){
         return new DirectExchange("directExchangeDemo2");
+        //调用了 this(name, true, false);   第二参数 durable 指定为了true,代表交换机是需要持久化的
     }
 
     @Bean
     public Queue directQueueDemo2(){
         return new Queue("directQueueDemo2");
+        //调用了重载构造方法 this(name, true, false, false);
+        //第二参数 durable 指定为了true,代表队列是需要持久化的
     }
 
     @Bean
