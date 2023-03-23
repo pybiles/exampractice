b98052e7759310066ae4c1b4ffb5e45942a1bee3
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-23 16:12:57 +0800

    RabbitMQ  确保消息不丢失的措施 ReturnCallback

--- Demos/RabbitDemo/src/main/java/com/yjx/RabbitMQConfig.java
+++ Demos/RabbitDemo/src/main/java/com/yjx/RabbitMQConfig.java
@@ -1,7 +1,7 @@
 package com.yjx;
 
 
-
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.InitializingBean;
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Configuration
-public class RabbitMQConfig implements InitializingBean{
+public class RabbitMQConfig implements InitializingBean {
 
     @Autowired
     RabbitTemplate rabbitTemplate;
@@ -24,11 +24,24 @@ public class RabbitMQConfig implements InitializingBean{
         rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
             @Override
             public void confirm(CorrelationData correlationData, boolean ack, String cause) {
-                System.out.println("correlationData "+correlationData);
-                System.out.println("ack 消息有没有正常到达MQServer "+ack);
-                System.out.println("cause ack为false时代表mq没有正常拿到消息的原因 "+cause);
+                System.out.println("correlationData " + correlationData);
+                System.out.println("ack 消息有没有正常到达MQServer " + ack);
+                System.out.println("cause ack为false时代表mq没有正常拿到消息的原因 " + cause);
             }
         });
+
+        //指定rabbitTemplate 无法正常到达队列的消息的强制退回
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                System.out.println("message " + message);
+                System.out.println("replyCode " + replyCode);
+                System.out.println("replyText " + replyText);
+                System.out.println("exchange " + exchange);
+                System.out.println("routingKey " + routingKey);
+            }
+        });
+        rabbitTemplate.setMandatory(true);  //补充设置
 
 
     }
