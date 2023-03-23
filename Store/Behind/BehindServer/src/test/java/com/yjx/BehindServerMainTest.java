d9ad6c2e0d23fbd12d31f0a45b9e72c2547e2528
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-23 22:11:54 +0800

    后台 测试通过map和通过sql查询

--- Store/Behind/BehindServer/src/test/java/com/yjx/BehindServerMainTest.java
+++ Store/Behind/BehindServer/src/test/java/com/yjx/BehindServerMainTest.java
@@ -1,5 +1,7 @@
 package com.yjx;
 
+import com.yjx.behind.model.Result;
+import com.yjx.dal.model.PermissionMenu;
 import com.yjx.service.ManagerService;
 import com.yjx.service.UrlPermissionService;
 import org.junit.jupiter.api.Test;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 public class BehindServerMainTest {
@@ -19,7 +22,15 @@ public class BehindServerMainTest {
         System.out.println(managerService.list());
     }
     @Test
-    void ulr(){
-        System.out.println(urlPermissionService.getAllRootMenuBySql());
+    void sqlAndMap(){
+        long a = System.currentTimeMillis();
+        List<PermissionMenu> allRootMenuBySql = urlPermissionService.getAllRootMenuBySql();
+        long b = System.currentTimeMillis();
+
+        List<PermissionMenu> allRootMenuByMap = urlPermissionService.getAllRootMenuByMap();
+        long c = System.currentTimeMillis();
+
+        System.out.println("allRootMenuBySql耗时 "+(b-a)+"ms  allRootMenuByMap耗时 "+(c-b)+"ms");
+
     }
 }
