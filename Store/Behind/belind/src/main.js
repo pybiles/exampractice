778b41fbd8626c107ff5585606e04000755ecd8a
pybiles <109141592+pybiles@users.noreply.github.com>
2023-03-23 19:17:30 +0800

    后台 前端 安装和导入axios npm i axios -S

--- Store/Behind/belind/src/main.js
+++ Store/Behind/belind/src/main.js
@@ -12,7 +12,12 @@ Vue.use(ElementUI);
 //引入全局CSS
 import './assets/css/global.css'
 import './assets/css/reset.css'
-
+//导入axios
+import axios from "axios";
+//绑定地址前缀
+axios.defaults.baseURL="http://localhost:9191/"
+//绑定到vue上
+Vue.prototype.$axios=axios;
 
 new Vue({
     router,
